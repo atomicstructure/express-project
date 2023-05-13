333814589ae7eb71b76e7ed19847788730f1792b
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-13 21:37:03 +0100

    Start of Express Http

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const app = express();
 const PORT = 3000;
 
 
+app.get('/', (re, res) => {
+    res.send({
+        id: 1,
+        name: 'Oluwafemi',
+        age: 37,
+        address: '23, Fagbeyiro Abayomi Street,Alakuko, Lagos'
+    });
+});
+
+app.get('/messages', (req, res) => {
+    res.send('<ul>Hello Oluwafemi Olusesi Ogundare</ul>')
+});
+
+app.post('/messages', (req, res) =>{
+    console.log('Updating messages...')
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
